6c35eee7b343e8b331c72ce6864ca205cc3b387f
Radi <radi.rusadi.s@gmail.com>
2018-06-06 20:45:11 +0700

    Add mapStateToProps connect method

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -4,9 +4,16 @@ import { connect } from 'react-redux';
 //create component list for display data to the listview
 class LibraryList extends Component {
     render () {
+        console.log(this.props);
         return;
     }
 }
 
+/*get state objek from global application inside redux store and take
+some properties and provide to props inside LibraryList*/
+const mapStateToProps = state => {
+    return { libraries: state.libraries };
+}
+
 //uses connect helper to get access to data state from library
-export default connect()(LibraryList);
+export default connect(mapStateToProps)(LibraryList);
