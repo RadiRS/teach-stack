8845f0a448bbff5b5ae918a82ea747c22ecba288
Radi <radi.rusadi.s@gmail.com>
2018-06-06 16:05:55 +0700

    Redux Boilerplate

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Main from "./src/Main";
 
 export default class App extends React.Component {
+  
   render() {
     return (
-      <View style={styles.container}>
-        <Text>The First Boot</Text>
+      <View>
+        <Main />
       </View>
     );
   }
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
