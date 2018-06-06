5230b55cf2cf25961990930e322df9f6a5659d34
Radi <radi.rusadi.s@gmail.com>
2018-06-06 21:17:47 +0700

    Listview setup

--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -1,19 +1,29 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { ListView } from "react-native";
+import { connect } from "react-redux";
 
 //create component list for display data to the listview
 class LibraryList extends Component {
-    render () {
-        console.log(this.props);
-        return;
-    }
+  componentWillMount() {
+    //create datasource to listview
+    const ds = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 != r2
+    });
+
+    //set datasource to use libraries at the listview
+    this.DataSource = ds.cloneWithRows(this.props.libraries);
+  }
+
+  render() {
+    return <ListView dataSource={this.dataSource} />;
+  }
 }
 
 /*get state objek from global application inside redux store and take
 some properties and provide to props inside LibraryList*/
 const mapStateToProps = state => {
-    return { libraries: state.libraries };
-}
+  return { libraries: state.libraries };
+};
 
 //uses connect helper to get access to data state from library
 export default connect(mapStateToProps)(LibraryList);
