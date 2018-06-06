4c1c3d953e5f2e79711bda78a9e2d8b0422ec90e
Radi <radi.rusadi.s@gmail.com>
2018-06-06 16:14:00 +0700

    Rendering the header

--- src/Main.js
+++ src/Main.js
@@ -3,11 +3,14 @@ import { View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "./reducers";
+import { Header } from "./components/common";
 
 const Main = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText="Tech Stack" />
+      </View>
     </Provider>
   );
 };
