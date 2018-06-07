5e8a1652c2cd19a05f8357434ba5718610f2bd82
Radi <radi.rusadi.s@gmail.com>
2018-06-07 13:02:20 +0700

    Call actions creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
-//action creator for selecte library
+/*action creator for selecte library
+action creator is a function a return actions,
+action contain of object with type property
+action tell reducer to update spesific data
+to export many function just use export dont use export default*/
+
 export const selectLibrary = (libraryId) => {
   return {
     type: "select_library",
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
 import { Text } from "react-native";
 import { CardSection } from "./common";
+import * as actions from '../actions';
 
+//create listitem component
 class ListItem extends Component {
   render() {
+    //destruct styles
     const { titleStyle } = styles;
-    
+    console.log(this.props);
+    //return component cardsection, text and get value of the props 
     return (
       <CardSection>
         <Text style={titleStyle}>
@@ -16,6 +21,7 @@ class ListItem extends Component {
   }
 }
 
+//create styles object
 const styles = {
   titleStyle: {
     fontSize: 18,
@@ -23,4 +29,5 @@ const styles = {
   }
 };
 
-export default ListItem;
+//return all action from actions and dispatch the action and pass to props in listitem
+export default connect(null, actions) (ListItem);
